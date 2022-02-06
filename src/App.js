50f506c93ad12dd75481e2e456b0afe63a702f2d
Kateryna-Urbanovych <katya_urb@ukr.net>
2022-02-06 15:47:09 +0200

    add project template

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "@progress/kendo-theme-default/dist/all.css";
+import "./App.css";
+
+export const App = () => {
+  return (
+    <div className='App'>
+      <h1>Hello KendoReact!</h1>
+    </div>
+  );
+};
